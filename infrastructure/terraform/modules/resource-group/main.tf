3c53d00d91f8b1842296e0ba5d4439ba51c95c2e
dodlaraghram <113520166+dodlaraghram@users.noreply.github.com>
2023-10-16 15:39:17 +0530

    modified for single instance

--- infrastructure/terraform/modules/resource-group/main.tf
+++ infrastructure/terraform/modules/resource-group/main.tf
@@ -1,5 +1,7 @@
-resource "azurerm_resource_group" "adl_rg" {
-  name     = "rg-${var.prefix}-${var.postfix}${var.env}"
-  location = var.location
-  tags     = var.tags
+data "azurerm_resource_group" "AIML_RG" {
+  name = "AIML_RG"
+}
+
+output "/subscriptions/ae231b2f-8211-4f88-b5c1-df0bf3fd97f0/resourceGroups/AIML_RG" {
+  value = data.azurerm_resource_group.AIML_RG.id
 }
