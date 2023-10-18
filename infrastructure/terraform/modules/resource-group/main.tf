bc0a29520cb8545d5a52103871e830e834508b0c
dodlaraghram <113520166+dodlaraghram@users.noreply.github.com>
2023-10-18 15:22:09 +0530

    Update main.tf

--- infrastructure/terraform/modules/resource-group/main.tf
+++ infrastructure/terraform/modules/resource-group/main.tf
@@ -2,6 +2,6 @@ data "azurerm_resource_group" "AIML_RG" {
   name = "AIML_RG"
 }
 
-output "/subscriptions/ae231b2f-8211-4f88-b5c1-df0bf3fd97f0/resourceGroups/AIML_RG" {
+output "id" {
   value = data.azurerm_resource_group.AIML_RG.id
 }
